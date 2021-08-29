53d8a9401a718d51da6da8631cd7924f6f08d871
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2021-08-28 22:59:09 -0600

    Initial commit on Develop_V1_0
    - Removed blink example from main

--- main.c
+++ main.c
@@ -6,22 +6,7 @@
 
 int main()
 {
-  RCC->AHB1ENR = 0x01 << 2;
-  
-  uint32_t delay = DELAY_VALUE;
-  GPIOC->MODER  = 0x01 << 26;
-  GPIOC->OTYPER &= ~(0x01 << 13);
-  GPIOC->PUPDR = 0x01 << 26;
-  
-  while(1)
-  {
-    GPIOC->ODR = 0x01 << 13;
-    for(delay = DELAY_VALUE; delay > 0; delay--);
-    GPIOC->ODR &= ~(0x01 << 13);
-    for(delay = DELAY_VALUE; delay > 0; delay--);
-  }
-  
-  
+  //Empty main for now
 }
 
 
