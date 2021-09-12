63437e9fa12242856a1085e046e72ca0e391fc88
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2021-09-12 04:18:51 -0600

    safety

--- Stepper_Driver/low_timer.c
+++ Stepper_Driver/low_timer.c
@@ -23,7 +23,7 @@ void ll_timer_tickrate_config(void)
     TIM2 setup to update CNT register at 100uS interval
   */
   TIM2->ARR = 0xFFFFFFFFUL;
-  TIM2->PSC = 0;
+  TIM2->PSC = 16;
   
   /*TIM2 count direction UP*/
   TIM2->CR1 &= ~(1 << 4);
@@ -63,10 +63,10 @@ void ll_timer_channel_config(void)
   TIM2->CCMR2 &= ~(0x07 << 12);
   
   /*Set initial output compare register to 100*/
-  TIM2->CCR1 = 8000000;
-  TIM2->CCR2 = 4000000;
-  TIM2->CCR3 = 16000000;
-  TIM2->CCR4 = 0;
+  TIM2->CCR1 = 15000;
+  TIM2->CCR2 = 5000;
+  TIM2->CCR3 = 5000;
+  TIM2->CCR4 = 10000;
   
   /*Enable interrupts*/
   TIM2->DIER |= (0x01 << 1);
@@ -83,50 +83,69 @@ void ll_timer_channel_config(void)
 
 void TIM2_IRQHandler(void)
 {
-  if(TIM2->SR & TIM_SR_CC1IF)
+  
+  TIM2->CR1 &= ~(1 << 0);
+  while(TIM2->SR & TIM_SR_CC1IF || TIM2->SR & TIM_SR_CC2IF || TIM2->SR & TIM_SR_CC3IF || TIM2->SR & TIM_SR_CC4IF)
   {
-    static uint32_t counter = 0;
-    uint32_t temp = TIM2->CCR1;
-    uint32_t tmp = temp;
-    TIM2->SR &= ~TIM_SR_CC1IF;
-    GPIOA->ODR |= (0x01 << 0);
-    GPIOA->ODR &= ~(0x01 << 0);
-    tmp = temp + ((2 * temp) / (4 * counter + 1));
-    TIM2->CCR1 = tmp;
-    counter++;
-  }
-  else if(TIM2->SR & TIM_SR_CC2IF)
-  {
-    static uint32_t counter = 0;
-    uint32_t temp = TIM2->CCR2;
-    uint32_t tmp = temp;
-    TIM2->SR &= ~TIM_SR_CC2IF;
-    GPIOA->ODR |= (0x01 << 1);
-    GPIOA->ODR &= ~(0x01 << 1);
-    tmp = temp + ((2 * temp) / (4 * counter + 1));
-    TIM2->CCR2 = tmp;
-    counter++;
-  }
-  else if(TIM2->SR & TIM_SR_CC3IF)
-  {
-    static uint32_t counter = 0;
-    uint32_t temp = TIM2->CCR3;
-    uint32_t tmp = temp;
-    TIM2->SR &= ~TIM_SR_CC3IF;
-    GPIOA->ODR |= (0x01 << 2);
-    GPIOA->ODR &= ~(0x01 << 2);
-    tmp = temp + ((2 * temp) / (4 * counter + 1));
-    TIM2->CCR2 = tmp;
-    counter++;
-  }
-  else if(TIM2->SR & TIM_SR_CC4IF)
-  {
-    TIM2->SR &= ~TIM_SR_CC4IF;
-    GPIOA->ODR |= (0x01 << 3);
-    GPIOA->ODR &= ~(0x01 << 3);
-    TIM2->CCR4 += 1600;
+    if(TIM2->SR & TIM_SR_CC1IF)
+    {
+      TIM2->SR &= ~TIM_SR_CC1IF;
+      
+      static uint32_t counter = 0;
+      uint32_t temp = TIM2->CCR1;
+      uint32_t tmp = temp;
+      
+      GPIOA->ODR |= (0x01 << 0);
+      GPIOA->ODR &= ~(0x01 << 0);
+      tmp = temp + ((2 * temp) / (4 * counter + 1));
+      TIM2->CCR1 = tmp;
+      counter++;
+    }
+    else if(TIM2->SR & TIM_SR_CC2IF)
+    {
+      TIM2->SR &= ~TIM_SR_CC2IF;
+      
+      static uint32_t counter = 0;
+      uint32_t temp = TIM2->CCR2;
+      uint32_t tmp = temp;
+      
+      GPIOA->ODR |= (0x01 << 1);
+      GPIOA->ODR &= ~(0x01 << 1);
+      tmp = temp + ((2 * temp) / (4 * counter + 1));
+      TIM2->CCR2 = tmp;
+      counter++;
+    }
+    else if(TIM2->SR & TIM_SR_CC3IF)
+    {
+      TIM2->SR &= ~TIM_SR_CC3IF;
+      
+      static uint32_t counter = 0;
+      uint32_t temp = TIM2->CCR3;
+      uint32_t tmp = temp;
+      
+      GPIOA->ODR |= (0x01 << 2);
+      GPIOA->ODR &= ~(0x01 << 2);
+      tmp = temp + ((2 * temp) / (4 * counter + 1));
+      TIM2->CCR3 = tmp;
+      counter++;
+    }
+    else if(TIM2->SR & TIM_SR_CC4IF)
+    {
+      TIM2->SR &= ~TIM_SR_CC4IF;
+      
+      static uint32_t counter = 0;
+      uint32_t temp = TIM2->CCR4;
+      uint32_t tmp = temp;
+      
+      GPIOA->ODR |= (0x01 << 3);
+      GPIOA->ODR &= ~(0x01 << 3);
+      tmp = temp + ((2 * temp) / (4 * counter + 1));
+      TIM2->CCR4 = tmp;
+      counter++;
+    }
   }
   NVIC_ClearPendingIRQ(TIM2_IRQn);
+  TIM2->CR1 |= (1 << 0);
 }
 
 
